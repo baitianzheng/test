48d2d346dec4dcfd69d0edeff3dc20f6b53de3ca
baitianzheng <79353313+baitianzheng@users.noreply.github.com>
2021-09-30 17:53:11 +0800

    Add files via upload
    第二天

---
+++ task_01day.py
@@ -0,0 +1,37 @@
+"""
+============================
+Author:柠檬班-木森
+Time:2020/2/5   19:35
+E-mail:3247119728@qq.com
+Company:湖南零檬信息技术有限公司
+============================
+"""
+"""
+1、下面那些不能作为变量？
+1、find     2、_num    3、7val        4、add.       5、def     
+6、pan      7、-print   8、open_file   9、FileName   10、9prints  
+11、INPUT   12、ls      13、user^name  14、list1     15、str_
+16、_888    17、is      18、true       19、none      20、try  
+
+    答案：7val   add.  def  -print  9prints user^name  is   try  
+    
+
+2、请描述一下变量的命名规范，（简单题）
+    1、只能由数字、字母、下划线组成，并且不能用数字开头
+    2、不能使用python中的关键字
+
+3、python如何如何注释代码
+    单行注释：使用 # 号 (pycharm的快捷键:ctrl+/)
+    文档注释(文档字符串)：使用三引号
+
+"""
+
+_num = 99
+
+# True = 99
+
+# true =999
+
+#  python中是严格区分大小写的
+t = 11
+T = 99
